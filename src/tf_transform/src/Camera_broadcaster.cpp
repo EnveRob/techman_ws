2e31179f32f83059b23051592d2444d323ddf3e0
nagio <railgun31325@gmail.com>
2023-03-24 17:21:10 +0800

    add camera link

--- src/tf_transform/src/Camera_broadcaster.cpp
+++ src/tf_transform/src/Camera_broadcaster.cpp
@@ -11,7 +11,7 @@
 void cameraCallback(const std_msgs::String::ConstPtr& msg)
 {
     ROS_INFO("I heard: [%s]", msg->data.c_str());
-    tf::TransformBroadcaster broadcaster; //创建tf广播器
+    static tf::TransformBroadcaster broadcaster; //创建tf广播器
     tf::Transform base2laser;
     std::stringstream ss(msg->data.c_str());
     std::string token;
@@ -24,14 +24,23 @@ void cameraCallback(const std_msgs::String::ConstPtr& msg)
         v.push_back(std::stod(token));
     }
     tf::Quaternion q;
+    ROS_INFO("I heard: [%f]", v[0]);
+    ROS_INFO("I heard: [%f]", v[1]);
+    ROS_INFO("I heard: [%f]", v[2]);
+    ROS_INFO("I heard: [%f]", v[3]);
+    ROS_INFO("I heard: [%f]", v[4]);
+    ROS_INFO("I heard: [%f]", v[5]);
+
     q.setRPY(v[3],v[4],v[5]);
+    // q.setRPY(0.05,0.05,0.01);
     base2laser.setRotation(q);              //设置旋转坐标
     base2laser.setOrigin(tf::Vector3(v[0],v[1],v[2]));//设平移坐标，mailbox在camera的(1,0,0)位置
-    ros::Rate loop_rate(10);
+    // base2laser.setOrigin(tf::Vector3(0,1,1));//设平移坐标，mailbox在camera的(1,0,0)位置
+    
     //循环发布坐标变换，两种方式
-    broadcaster.sendTransform(tf::StampedTransform(base2laser,ros::Time::now(),"gripper","mailbox"));
-    //broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 0), tf::Vector3(1, 0.0, 0)),ros::Time::now(),"base_link", "base_laser"));
-    loop_rate.sleep();
+    broadcaster.sendTransform(tf::StampedTransform(base2laser,ros::Time::now(),"camera","mailbox"));
+    //broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0s, 0, 0, 0), tf::Vector3(1, 0.0, 0)),ros::Time::now(),"base_link", "base_laser"));
+    // loop_rate.sleep();
     
 }
 
@@ -39,14 +48,11 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "camera_broadcaster");//初始化ROS节点与节点名称
     ros::NodeHandle n;                    //创建节点的句柄 
+    // ros::Rate loop_rate(10);
     
-    
-    ros::Subscriber sub = n.subscribe("camera_data", 10, cameraCallback);
-               //控制节点运行的频率,与loop.sleep共同使用
+    ros::Subscriber sub = n.subscribe("camera_data", 10, &cameraCallback);
+    //控制节点运行的频率,与loop.sleep共同使用
 
-    
-    
-    // loop_rate.sleep();
     ros::spin();  
     return 0;
 }
