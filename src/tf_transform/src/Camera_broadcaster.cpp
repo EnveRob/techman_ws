3767fe257b9c471354d4bd22d0e1c92b15d491be
William <williampai0704@gmail.com>
2023-04-21 20:15:58 +0200

    Revert "debug cmake"
    This reverts commit 2431fb92887549e1ab547ee0bcb186a258159f48.

--- src/tf_transform/src/Camera_broadcaster.cpp
+++ src/tf_transform/src/Camera_broadcaster.cpp
@@ -34,11 +34,7 @@ void cameraCallback(const std_msgs::String::ConstPtr& msg)
 =======
     tf::Transform base2laser;
     tf::Quaternion q;
-<<<<<<< HEAD
-    q.setRPY(.5,0,-0.5);
-=======
     q.setRPY(0,0,0.2);
->>>>>>> parent of 2431fb9... debug cmake
     base2laser.setRotation(q);              //设置旋转坐标
 <<<<<<< HEAD
     base2laser.setOrigin(tf::Vector3(-1,-3,-3));//设平移坐标，mailbox在camera的(1,0,0)位置
