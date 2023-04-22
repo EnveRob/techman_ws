8128d3ca20d51c16a480a3c7cd556daf55957bd8
William <williampai0704@gmail.com>
2023-04-22 13:05:09 +0200

    solve merge conflict

--- src/tf_transform/src/Camera_broadcaster.cpp
+++ src/tf_transform/src/Camera_broadcaster.cpp
@@ -1,19 +1,11 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-<<<<<<< HEAD
 #include "std_msgs/String.h"
 #include <string>
 #include <sstream>
 #include <vector>
 #include <iostream>
 const double gripper_offset = 0.135;
-=======
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "camera_broadcaster");//初始化ROS节点与节点名称
-    ros::NodeHandle n;                    //创建节点的句柄
-    ros::Rate loop_rate(10);             //控制节点运行的频率,与loop.sleep共同使用
->>>>>>> 618bf74a4b30409da0ffaee2906967414eb9cab7
 
 
 void cameraCallback(const std_msgs::String::ConstPtr& msg)
@@ -30,24 +22,6 @@ void cameraCallback(const std_msgs::String::ConstPtr& msg)
     for (const auto& token : tokens) {
         v.push_back(std::stod(token));
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    tf::Transform base2laser;
-    tf::Quaternion q;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    q.setRPY(0,0,0.2);
-=======
-    q.setRPY(.5,0,-0.5);
->>>>>>> parent of aad1844... stage temperor
-    base2laser.setRotation(q);              //设置旋转坐标
-    base2laser.setOrigin(tf::Vector3(-1,-3,-3));//设平移坐标，mailbox在camera的(1,0,0)位置
->>>>>>> add tf_transform/src/moveit_demo
-=======
->>>>>>> parent of d08365e... add tf_transform/src/moveit_demo
-=======
->>>>>>> parent of d08365e... add tf_transform/src/moveit_demo
 
     ROS_INFO("I heard: [%f]", v[0]);
     ROS_INFO("I heard: [%f]", v[1]);
@@ -55,11 +29,6 @@ void cameraCallback(const std_msgs::String::ConstPtr& msg)
     ROS_INFO("I heard: [%f]", v[3]);
     ROS_INFO("I heard: [%f]", v[4]);
     ROS_INFO("I heard: [%f]", v[5]);
-=======
-    q.setRPY(0,0,0.2);
-    base2laser.setRotation(q);              //设置旋转坐标
-    base2laser.setOrigin(tf::Vector3(0.1,0.3,0.3));//设平移坐标，mailbox在camera的(1,0,0)位置
->>>>>>> 618bf74a4b30409da0ffaee2906967414eb9cab7
 
     static tf::TransformBroadcaster broadcaster; //创建tf广播器
     tf::Transform mail2camera;
