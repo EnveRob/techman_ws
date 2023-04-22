d4c6115898fdc17ab5e3954259c55f76ebff37a7
a891021960035 <a891021960035@gmail.com>
2023-04-22 23:03:32 +0800

    Test new way of broadcaster (Preparing for feedback_controller) delete camera_broadcaster_test

--- src/feedback_controller/src/feedback_controller.cpp
+++ src/feedback_controller/src/feedback_controller.cpp
@@ -1,7 +1,12 @@
 #include "ros/ros.h"
+#include "std_msgs/String.h"
 #include "geometry_msgs/Vector3.h"
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
 
 const double TARGET_FORCE_Y = 330.0;
 const double TARGET_FORCE_Y_THRESHOLD = 20.0;
@@ -47,28 +52,73 @@ void move_arm(moveit::planning_interface::MoveGroupInterface &move_group, double
 
     // 获取机械臂的当前姿态
     geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose(end_effector_link);
-    //輸出current_pose
-    ROS_INFO("Current Pose: %f, %f, %f, %f", current_pose.pose.orientation.w, current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z);
 
     current_pose.pose.position.y += MOVEMENT_STEP;
     move_group.setPoseTarget(current_pose, end_effector_link);
     move_group.move();
 
-    // Rotate the end effector 10 degrees around the x-axis of the gripper_link
-    current_pose = move_group.getCurrentPose(end_effector_link);
-    //輸出current_pose
-    ROS_INFO("Current Pose: %f, %f, %f, %f", current_pose.pose.orientation.w, current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z);
-    // rotated_pose.pose.position.y -= 0.02;
-    // rotated_pose.pose.position.x += 0.005;
-    // rotated_pose.pose.position.z += 0.005;
-    current_pose.pose.orientation.x -= 0.087;
-    current_pose.pose.orientation.w += 0.004;
-    std::cout << "current_pose.pose.orientation.x: " << current_pose.pose.orientation.x << std::endl;
-    std::cout << "current_pose.pose.orientation.w: " << current_pose.pose.orientation.w << std::endl;
-    move_group.setPoseTarget(current_pose, end_effector_link);
+    // --------------------------------------------------------------------------------
+    // // Rotate the end effector 10 degrees around the x-axis of the gripper_link
+    // current_pose = move_group.getCurrentPose(end_effector_link);
+    // //輸出current_pose
+    // ROS_INFO("Current Pose: %f, %f, %f, %f", current_pose.pose.orientation.w, current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z);
+    // // rotated_pose.pose.position.y -= 0.02;
+    // // rotated_pose.pose.position.x += 0.005;
+    // // rotated_pose.pose.position.z += 0.005;
+    // current_pose.pose.orientation.x -= 0.087;
+    // current_pose.pose.orientation.w += 0.004;
+    // std::cout << "current_pose.pose.orientation.x: " << current_pose.pose.orientation.x << std::endl;
+    // std::cout << "current_pose.pose.orientation.w: " << current_pose.pose.orientation.w << std::endl;
+    // move_group.setPoseTarget(current_pose, end_effector_link);
+    // move_group.move();
+
+    
+    // --------------------------------------------------------------------------------    
+    tf::TransformBroadcaster br; //创建tf广播器
+    tf::TransformListener listener;
+
+    // 定義一個固定的轉換 
+    tf::Transform gripperRotaion;
+    tf::Quaternion q;
+    q.setRPY(ROTATION_STEP, 0, 0);
+    gripperRotaion.setRotation(q);
+
+    // 發布轉換
+    br.sendTransform(tf::StampedTransform(gripperRotaion,ros::Time::now(),"gripper_link","rotated_frame"));
+
+    // 設置轉換
+    tf::StampedTransform rotaionTransform;
+    listener.lookupTransform("base", "rotated_frame", ros::Time(0), rotaionTransform);
+    ROS_INFO("Transform: \n %.2f, %.2f, %.2f, \n %.2f, %.2f, %.2f, %.2f)",
+              rotaionTransform.getOrigin().x(),
+              rotaionTransform.getOrigin().y(),
+              rotaionTransform.getOrigin().z(),
+              rotaionTransform.getRotation().w(),
+              rotaionTransform.getRotation().x(),
+              rotaionTransform.getRotation().y(),
+              rotaionTransform.getRotation().z());
+
+    // 宣告儲存gripper幾何姿態的變數
+    geometry_msgs::PoseStamped rotated_pose;
+    rotated_pose.pose.position.x = rotaionTransform.getOrigin().x();
+    rotated_pose.pose.position.y = rotaionTransform.getOrigin().y();
+    rotated_pose.pose.position.z = rotaionTransform.getOrigin().z();
+    rotated_pose.pose.orientation.w = rotaionTransform.getRotation().w();
+    rotated_pose.pose.orientation.x = rotaionTransform.getRotation().x();
+    rotated_pose.pose.orientation.y = rotaionTransform.getRotation().y();
+    rotated_pose.pose.orientation.z = rotaionTransform.getRotation().z();
+    
+    move_group.setPoseTarget(rotated_pose, end_effector_link);
     move_group.move();
 
     current_pose = move_group.getCurrentPose(end_effector_link);
     //輸出current_pose
-    ROS_INFO("Current Pose: %f, %f, %f, %f", current_pose.pose.orientation.w, current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z);
+    ROS_INFO("Transform: \n %.2f, %.2f, %.2f, \n  %.2f, %.2f, %.2f, %.2f)",
+              current_pose.pose.position.x, 
+              current_pose.pose.position.y, 
+              current_pose.pose.position.z,
+              current_pose.pose.orientation.w, 
+              current_pose.pose.orientation.x, 
+              current_pose.pose.orientation.y, 
+              current_pose.pose.orientation.z);
 }
--- src/tf_transform/src/Camera_broadcaster.cpp
+++ src/tf_transform/src/Camera_broadcaster.cpp
@@ -1,10 +1,10 @@
 #include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
 #include "std_msgs/String.h"
-#include <string>
-#include <sstream>
-#include <vector>
-#include <iostream>
+#include <tf/transform_broadcaster.h>
+
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+
 const double gripper_offset = 0.135;
 
 
@@ -30,23 +30,48 @@ void cameraCallback(const std_msgs::String::ConstPtr& msg)
     ROS_INFO("I heard: [%f]", v[4]);
     ROS_INFO("I heard: [%f]", v[5]);
 
-    static tf::TransformBroadcaster broadcaster; //创建tf广播器
-    tf::Transform mail2camera;
+    // --------------------------------------------------------------------------------
+    // static tf::TransformBroadcaster broadcaster; //创建tf广播器
+    // tf::Transform mail2camera;
+    // tf::Quaternion q;
+    // q.setRPY(v[3],v[4],v[5]);                   //以弧度為單位
+    // mail2camera.setRotation(q);                 //设置旋转坐标
+    // mail2camera.setOrigin(tf::Vector3(v[0],v[1],v[2]-gripper_offset));//设平移坐标，mailbox在camera的(1,0,0)位置
+    //mail2camera.setOrigin(tf::Vector3(0,1,1));//设平移坐标，mailbox在camera的(1,0,0)位置
+    // ros::Time start_time = ros::Time::now();
+    // //循环发布坐标变换，两种方式
+    // while(ros::Time::now()-start_time <= ros::Duration(5))
+    // {
+    //     broadcaster.sendTransform(tf::StampedTransform(mail2camera,ros::Time::now(),"camera","mailbox"));
+    //     //broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 0), tf::Vector3(1, 0.0, 0)),ros::Time::now(),"base_link", "base_laser"));
+    //     ROS_INFO("finish broadcast");
+    //     ros::Duration(0.1).sleep();
+    // }
+
+    // --------------------------------------------------------------------------------
+    static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+    geometry_msgs::TransformStamped mail2camera;
+
+    // 创建一个 tf::Transform 对象
+    // tf::Transform mail2camera;
     tf::Quaternion q;
     q.setRPY(v[3],v[4],v[5]);                   //以弧度為單位
-    mail2camera.setRotation(q);                 //设置旋转坐标
-    mail2camera.setOrigin(tf::Vector3(v[0],v[1],v[2]-gripper_offset));//设平移坐标，mailbox在camera的(1,0,0)位置
-    //mail2camera.setOrigin(tf::Vector3(0,1,1));//设平移坐标，mailbox在camera的(1,0,0)位置
-    ros::Time start_time = ros::Time::now();
-    //循环发布坐标变换，两种方式
-    while(ros::Time::now()-start_time <= ros::Duration(5))
-    {
-        broadcaster.sendTransform(tf::StampedTransform(mail2camera,ros::Time::now(),"camera","mailbox"));
-        //broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 0), tf::Vector3(1, 0.0, 0)),ros::Time::now(),"base_link", "base_laser"));
-        ROS_INFO("finish broadcast");
-        ros::Duration(0.1).sleep();
-    }
-    
+    mail2camera.transform.rotation.x = q.x();
+    mail2camera.transform.rotation.y = q.y();
+    mail2camera.transform.rotation.z = q.z();
+    mail2camera.transform.rotation.w = q.w();
+    mail2camera.transform.translation.x = v[0];
+    mail2camera.transform.translation.y = v[1];
+    mail2camera.transform.translation.z = v[2]-gripper_offset;
+
+    // 设置 transform 的时间戳、参考系名称和子参考系名称
+    mail2camera.header.stamp = ros::Time::now();
+    mail2camera.header.frame_id = "camera";
+    mail2camera.child_frame_id = "mailbox";
+
+    // 发布 transform
+    static_broadcaster.sendTransform(mail2camera);
+
 }
 
 int main(int argc, char** argv)
